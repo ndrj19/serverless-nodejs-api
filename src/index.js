dc89b0aa16472d48217b87501fcc297ccf8f0581
Andrej Hagenmüller <andrej.hagenmueller@gmail.com>
2024-03-01 12:05:22 +0000

    decouple and upgrade AWS SSM Client

--- src/index.js
+++ src/index.js
@@ -1,36 +1,16 @@
 const serverless = require("serverless-http");
 const express = require("express");
-const AWS = require("aws-sdk");
-const { neon, neonConfig } = require("@neondatabase/serverless");
-
-const AWS_REGION = "eu-west-2";
-const STAGE = process.env.STAGE || "prod";
-
+const { getDbClient } = require("./db/clients");
 const app = express();
-const ssm = new AWS.SSM({ region: AWS_REGION });
-
-const DATABASE_URL_SSM_PARAM = `/serverless-nodejs-api/${STAGE}/database-url`;
-
-async function dbClient() {
-  // for HTTP connections, non-pooling
-  const paramStoreData = await ssm
-    .getParameter({
-      Name: DATABASE_URL_SSM_PARAM,
-      WithDecryption: true,
-    })
-    .promise();
-  // console.log(paramStoreData.Parameter.Value);
-  neonConfig.fetchConnectionCache = true;
-  const sql = neon(paramStoreData.Parameter.Value);
-  return sql;
-}
 
 app.get("/", async (req, res, next) => {
-  const sql = await dbClient();
-  const [results] = await sql`select now();`;
+  const sql = await getDbClient();
+  const now = Date.now();
+  const [dbNowResult] = await sql`select now();`;
+  const delta = (dbNowResult.now.getTime() - now) / 1000;
   return res.status(200).json({
     message: "Hello from the top of the mountain!",
-    results: results.now,
+    results: delta,
   });
 });
 
